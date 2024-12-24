f532542a9f84a1bdd35016133f17064a33fe7885
Sai-venkata-raghavendra <sairaghavendra121@gmail.com>
2024-12-24 08:17:28 +0530

    Update SwaggerConfig.java

--- infosysdemo/src/main/java/com/infosys/project/infosysdemo/config/SwaggerConfig.java
+++ infosysdemo/src/main/java/com/infosys/project/infosysdemo/config/SwaggerConfig.java
@@ -1,25 +1,27 @@
-/*
- * package com.infosys.project.infosysdemo.config;
- * 
- * import org.springframework.context.annotation.Bean; import
- * org.springframework.context.annotation.Configuration;
- * 
- * import io.swagger.v3.oas.models.Components; import
- * io.swagger.v3.oas.models.OpenAPI; import io.swagger.v3.oas.models.info.Info;
- * import io.swagger.v3.oas.models.security.SecurityRequirement; import
- * io.swagger.v3.oas.models.security.SecurityScheme;
- * 
- * @Configuration public class SwaggerConfig {
- * 
- * @Bean public OpenAPI customOpenAPI() {
- * 
- * return new OpenAPI().info(new Info().title("Employee Data"))
- * .addSecurityItem(new SecurityRequirement().addList("EmployeeSchema"))
- * .components(new Components().addSecuritySchemes("EmployeeSecurityScheme", new
- * SecurityScheme().name("EmployeeSecuritySecurityScheme")
- * .type(SecurityScheme.Type.HTTP) .scheme("bearer").bearerFormat("JWT")));
- * 
- * }
- * 
- * }
- */
+
+package com.infosys.project.infosysdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+
+		return new OpenAPI().info(new Info().title("Employee Data"))
+				.addSecurityItem(new SecurityRequirement().addList("EmployeeSchema"))
+				.components(new Components().addSecuritySchemes("EmployeeSecurityScheme",
+						new SecurityScheme().name("EmployeeSecuritySecurityScheme").type(SecurityScheme.Type.HTTP)
+								.scheme("bearer").bearerFormat("JWT")));
+
+	}
+
+}
